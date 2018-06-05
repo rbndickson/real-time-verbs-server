4da7c9f18be280722cec9240ca5b6d77b76fcdc9
Robin Dickson <robindickson@gmail.com>
2018-06-05 15:50:32 +0700

    feat: Receive and emit quesions

--- server.js
+++ server.js
@@ -9,9 +9,16 @@ const io = socketIO(server);
 
 io.on("connection", socket => {
   console.log("User connected");
+  socket.join("main");
 
   socket.on("disconnect", () => {
     console.log("user disconnected");
+  });
+
+  socket.on("question", data => {
+    console.log(data);
+
+    socket.broadcast.to("main").emit("receive question", data);
   });
 });
 
